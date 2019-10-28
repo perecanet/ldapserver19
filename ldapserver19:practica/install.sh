7e441c7b8a289a9e93c61530ed7c28c1acb7f887
PereCanet <pere6cp@gmail.com>
2019-10-28 09:31:07 +0100

    practica

--- ldapserver19:practica/install.sh
+++ ldapserver19:practica/install.sh
@@ -6,6 +6,7 @@ rm -rf /var/lib/ldap/*
 cp /opt/docker/DB_CONFIG /var/lib/ldap/.
 slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
 slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.org.usuaris.ldif
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/estudiants.ldif
 chown -R ldap.ldap /etc/openldap/slapd.d
 chown -R ldap.ldap /var/lib/ldap
 cp /opt/docker/ldap.conf /etc/openldap/.
